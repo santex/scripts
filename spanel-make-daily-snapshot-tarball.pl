4ca5d3006201474dd2196e015ee93cd1a1d88f79
Steven Haryanto <steven@masterwebnet.com>
2007-10-25 20:51:09 +0700

    add note about bzr checkout

--- spanel-make-daily-snapshot-tarball.pl
+++ spanel-make-daily-snapshot-tarball.pl
@@ -16,6 +16,13 @@ my $timestamp = strftime "%Y%m%d", localtime;
 my $targetn = "spanel-$Spanel::VERSION-$timestamp";
 my $targetp = "$TARGET_DIR/$targetn";
 
+# this would be more appropriate as it excludes uncommited changes,
+# but it's slooow. so we'll still use rsync for now:
+#
+#   chdir $PROJ_DIR or die "FATAL: Can't chdir to `$PROJ_DIR': $!\n";
+#   bzr checkout . $targetp/ && \
+#   cd $TARGET_DIR && rm -rf .bzr && \
+
 system qq(rsync -a --del --exclude '*~' --exclude '.bzr' $PROJ_DIR/ $targetp/ && \
   cd $TARGET_DIR && \
   rm -f $targetn.tar.gz && \
