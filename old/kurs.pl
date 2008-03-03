488a752d828ab8456d69fbd1acbe381d906f2d56
User One <user1@carnatic>
2008-03-03 11:13:34 +0700

    delete unneeded scripts

--- old/kurs.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-use LWP::Simple; 
-($s,$t)=@ARGV or die "Usage: $0 FROM TO\neg: $0 USD IDR\n"; 
-($_ = get "http://finance.yahoo.com/m5?a=1&s=$s&t=$t") =~ 
-  m#<th nowrap>(\d+:\d+[ap]m)</th><td>(\d+\.\d+)</td># or die "Failed\n\n$_";
-print "1 $s = $2 $t ($1)\n";
